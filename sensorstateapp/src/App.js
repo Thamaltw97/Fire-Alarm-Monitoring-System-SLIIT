1a7a5fc750ef2938bec5d9976b954b25dac31192
Thamaltw97 <thamal1997@gmail.com>
2020-04-28 17:17:12 +0530

    Starting Sensor App

---
+++ sensorstateapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component
+{
+
+  //About to start
+
+  render() {
+    return (
+        <div>
+          <h2 align="center">Sensor Updating App is running...</h2>
+        </div>
+    );
+  }
+}
+
+export default App;
