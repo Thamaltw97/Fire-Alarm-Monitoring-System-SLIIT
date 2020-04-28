eab3cbd5f9a8bf64eb4764220e06e0d96829ad5a
Thamaltw97 <thamal1997@gmail.com>
2020-04-28 17:27:03 +0530

    Updating-OneSensor-10seconds

--- sensorstateapp/src/App.js
+++ sensorstateapp/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-class App extends React.Component
-{
+class App extends React.Component {
 
-  //About to start
+
+    //This helps to call handleSensorUpdate() function in every 10 seconds
+    componentWillMount () {
+        this.handleSensorUpdate();
+        setInterval(this.handleSensorUpdate, 10000);
+    }
+
+    //Main function to update smoke and co2 levels
+    handleSensorUpdate () {
+
+        //Passing POST Json objects to each sensor to update levels
+        const obj = {
+            sensorId: 1,
+            smokeLevel: Math.floor(Math.random() * 10) + 1,
+            coLevel: Math.floor(Math.random() * 10) + 1
+        };
+        axios
+            .post('http://localhost:44381/api/sensor/setsensorlevel', obj)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+
+    }
 
   render() {
     return (
